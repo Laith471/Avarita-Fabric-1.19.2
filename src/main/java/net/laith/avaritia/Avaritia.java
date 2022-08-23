2b510c92167d8749cc5ce31fb791cb8959e1746c
Laith471 <liathaqeel@gmail.com>
2022-08-23 17:45:27 +0300

    Initial commit

---
+++ src/main/java/net/laith/avaritia/Avaritia.java
@@ -0,0 +1,16 @@
+package net.laith.avaritia;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Avaritia implements ModInitializer {
+
+	public static final String MOD_ID = "avaritia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/laith/avaritia/AvaritiaClient.java
@@ -0,0 +1,10 @@
+package net.laith.avaritia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AvaritiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
